48e2372f5e094918b17a64a0763df9f4140eb3b5
dwidge <dwidge+gh@gmail.com>
2025-01-27 08:27:12 +0200

    Add param useConvert toT prevT

--- src/useConvert.ts
+++ src/useConvert.ts
@@ -9,26 +9,27 @@ import { AsyncDispatch, AsyncState } from "./State.js";
  * @template T The type of the original state value.
  * @template R The type of the derived state value. Defaults to T if not specified.
  * @param {(t: T) => R} toR A function that converts the original state value (type `T`) to the derived state value (type `R`).
- * @param {(r: R) => T} toT A function that converts the derived state value (type `R`) back to the original state value (type `T`).
+ * @param {(r: R, prevT?: T) => T} toT A function that converts the derived state value (type `R`) back to the original state value (type `T`), optionally receiving the previous original state `prevT` to facilitate merging or partial updates.
  * @returns {(state: AsyncState<T>) => AsyncState<R>} A function that accepts an `AsyncState` tuple of type `T` and returns a derived `AsyncState` tuple of type `R`.
  */
 export const useConvert =
-  <T, R = T>(toR: (t: T) => R, toT: (r: R) => T) =>
-  ([t, setT]: AsyncState<T>): AsyncState<R> => {
-    const r = t === undefined ? undefined : toR(t);
+  <T, R = T>(toR: (t: T) => R, toT: (r: R, prevT: T) => T) =>
+    ([t, setT]: AsyncState<T>): AsyncState<R> => {
+      const r = t === undefined ? undefined : toR(t);
 
-    const setR = useMemo(
-      () =>
-        setT
-          ? ((async (getR) => {
-              const newR = getR instanceof Function ? getR(r!) : getR;
-              const t: T = toT(newR);
-              await setT(t);
-              return newR;
-            }) as AsyncDispatch<R>)
-          : undefined,
-      [r, setT],
-    );
+      const setR = useMemo(
+        () =>
+          setT && t !== undefined
+            ? ((async (getR) => {
+                const newRValue: R =
+                  getR instanceof Function ? getR(r as R) : getR;
+                const tValue: T = toT(newRValue, t);
+                await setT(tValue);
+                return newRValue;
+              }) as AsyncDispatch<R>)
+            : undefined,
+        [r, setT, toT, t],
+      );
 
-    return [r, setR];
-  };
+      return [r, setR];
+    };
