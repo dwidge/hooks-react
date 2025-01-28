116dfcb0523141ee38f05d35e1e1ec2a5aef52d1
dwidge <dwidge+gh@gmail.com>
2025-01-28 19:38:46 +0200

    Add useMemo to useAsync

--- src/useAsync.ts
+++ src/useAsync.ts
@@ -2,29 +2,36 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export const useAsync = <F extends (...args: any[]) => Promise<any>>(
-  f: F | undefined
-): [f: F | undefined, busy: boolean, error: Error | undefined] => {
+  f: F | undefined,
+): [f: F | undefined, busy: boolean, error: Error | null] => {
   const [busy, setBusy] = useState(false);
-  const [error, setError] = useState<Error>();
+  const [error, setError] = useState<Error | null>(null);
 
-  const wrappedFunction =
-    f &&
-    (async (...args: Parameters<F>): Promise<ReturnType<F>> => {
-      setBusy(true);
-      setError(undefined);
-      try {
-        const result = await f(...args);
-        return result;
-      } catch (e) {
-        setError(e as Error);
-        throw e;
-      } finally {
-        setBusy(false);
-      }
-    });
+  const wrappedFunction = useMemo(
+    () =>
+      f
+        ? ((async (...args) => {
+            setBusy(true);
+            setError(null);
+            try {
+              const result = await f(...args);
+              return result;
+            } catch (e) {
+              setError(e as Error);
+              throw e;
+            } finally {
+              setBusy(false);
+            }
+          }) as F)
+        : undefined,
+    [f],
+  );
 
-  return [wrappedFunction as F, busy, error];
+  return useMemo(
+    () => [wrappedFunction, busy, error],
+    [wrappedFunction, busy, error],
+  );
 };
