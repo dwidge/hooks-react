36b4b4e22428538571d16b44178eb8a6d74c3232
dwidge <dwidge+gh@gmail.com>
2025-02-17 21:57:30 +0200

    Add result and default args to useAsync

--- src/useAsync.ts
+++ src/useAsync.ts
@@ -2,23 +2,35 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { useDeepMemo } from "./useDeepMemo.js";
 
 export const useAsync = <F extends (...args: any[]) => Promise<any>>(
   f: F | undefined,
-): [f: F | undefined, busy: boolean, error: Error | null] => {
+  args?: Parameters<F>,
+): [
+  wrappedFunction: F | undefined,
+  busy: boolean,
+  error: Error | undefined,
+  result: Awaited<ReturnType<F>> | undefined,
+] => {
   const [busy, setBusy] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<Error | undefined>(undefined);
+  const [result, setResult] = useState<Awaited<ReturnType<F>> | undefined>(
+    undefined,
+  );
 
   const wrappedFunction = useMemo(
     () =>
       f
-        ? ((async (...args) => {
+        ? ((async (...args: Parameters<F>) => {
             setBusy(true);
-            setError(null);
+            setError(undefined);
+            setResult(undefined);
             try {
-              const result = await f(...args);
-              return result;
+              const executionResult = await f(...args);
+              setResult(executionResult);
+              return executionResult;
             } catch (e) {
               setError(e as Error);
               throw e;
@@ -30,8 +42,17 @@ export const useAsync = <F extends (...args: any[]) => Promise<any>>(
     [f],
   );
 
+  // Initial call if arg is provided
+  useEffect(() => {
+    if (args !== undefined && wrappedFunction) {
+      wrappedFunction(...args).catch(() => {
+        // Error is already handled by wrappedFunction and setError state.
+      });
+    }
+  }, [wrappedFunction, useDeepMemo(args !== undefined ? args : null)]);
+
   return useMemo(
-    () => [wrappedFunction, busy, error],
-    [wrappedFunction, busy, error],
+    () => [wrappedFunction, busy, error, result],
+    [wrappedFunction, busy, error, result],
   );
 };
